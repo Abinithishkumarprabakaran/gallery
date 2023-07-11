3949be71002eb11a0e62c3f6d701f69b518227e5
Abinithishkumarprabakaran <abinithishkumarp.a.k@gmail.com>
2023-07-11 11:27:58 +0530

    loginpage-updated

--- src/credentials/LoginPage.vue
+++ src/credentials/LoginPage.vue
@@ -1,51 +1,52 @@
 <template>
-  <v-form v-model="valid">
-
-    <v-container fluid class="d-flex">
-      <h1 class="justify-center">Login Page</h1>
-    </v-container>
-
-    <v-container>
-      <v-row>
-        <v-col
-          cols="12"
-          md="4"
-        >
-          <v-text-field
-            v-model="email"
-            :rules="emailRules"
-            label="E-mail"
-            required
-          ></v-text-field>
-        </v-col>
-
-        <v-col
-          cols="12"
-          md="4"
-        >
-          <v-text-field
-            v-model="password"
-            :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
-            :rules="passwordRules"
-            :type="show1 ? 'text' : 'password'"
-            name="input-10-1"
-            label="Password"
-            @click:append="show1 = !show1"
-          ></v-text-field>
-        </v-col>
-
-      </v-row>
-
-      <v-btn 
-        block
-        elevation="2"
-        @click.prevent="submitLoginForm"
+  <div class="login-card">
+    <v-form v-model="valid">
+      <v-card
+        class="mx-auto my-12"
+        width="300"
       >
-        Submit
-      </v-btn>
-
-    </v-container>
-  </v-form>
+        <div class="card-container">
+          <v-card-title>
+            <h2>Login</h2>
+          </v-card-title>
+          <v-card-text>
+            <v-row>
+              <v-col cols="12" class="text-field">
+                <v-text-field
+                  v-model="email"
+                  :rules="emailRules"
+                  label="E-mail"
+                  required
+                ></v-text-field>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col cols="12" class="text-field">
+                <v-text-field
+                  v-model="password"
+                  :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+                  :rules="passwordRules"
+                  :type="show1 ? 'text' : 'password'"
+                  name="input-10-1"
+                  label="Password"
+                  @click:append="show1 = !show1"
+                ></v-text-field>
+              </v-col>
+            </v-row>
+          </v-card-text>
+          <v-card-actions class="card-actions">
+            <v-btn
+              block
+              elevation="2"
+              @click.prevent="submitLoginForm"
+            >
+              Log in
+            </v-btn>
+          </v-card-actions>
+        </div>
+      </v-card>
+    </v-form>
+  </div>
 </template>
 
 <script>
@@ -99,3 +100,21 @@ import axios from 'axios';
     }
   }
 </script>
+
+<style scoped>
+
+  .login-card {
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .card-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+</style>
